969a444529188d929f5cd52685b5d777e1200242
jonoerik <jonoerik@fastmail.fm>
2014-04-23 23:45:51 +1000

    You can now quit the game. How exciting!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "libtcod.hpp"
 #include "Map.hpp"
+#include <iostream> //TODO remove
 
 static size_t width = 150;
 static size_t height = 80;
@@ -13,6 +14,17 @@ int main(int argc, char* argv[]) {
     while (running && !TCODConsole::isWindowClosed()) {
 	map.Render();
 	TCODConsole::flush();
+
+	TCOD_key_t key;
+	do {
+	    key = TCODConsole::checkForKeypress();
+	    switch (key.vk) {
+	    case TCODK_ESCAPE:
+		running = false;
+		std::cout << "esc" << std::endl;
+		break;
+	    }
+	} while (key.vk != TCODK_NONE);
     }
     return 0;
 }
